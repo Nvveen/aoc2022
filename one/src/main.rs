c8928e3846abd22f2366fb2c98bcc11fd0773ee0
Neal van Veen <nealvanveen@gmail.com>
2022-12-16 16:04:18 +0000

    finish part one

--- one/src/main.rs
+++ one/src/main.rs
@@ -7,5 +7,33 @@ fn main() {
 
     let contents = fs::read_to_string(FILENAME).expect("Should have been able to read the input");
 
-    println!("With text:\n{contents}");
+    // Split the input into lines
+    let lines = contents.split('\n');
+
+    // Group lines per empty line
+    let groups: Vec<i32> = lines
+        .into_iter()
+        .fold((Vec::new(), Vec::new()), |(mut groups, mut group), line| {
+            if line.is_empty() {
+                let sum: i32 = group.iter().sum();
+                groups.push(sum);
+                (groups, Vec::new())
+            } else {
+                let number: i32 = line
+                    .parse()
+                    .expect("Should have been able to parse the number");
+                group.push(number);
+                (groups, group)
+            }
+        })
+        .0;
+
+    // Get the largest number in groups
+    let largest = groups
+        .iter()
+        .max()
+        .expect("Should have been able to get the largest number");
+
+    // print result
+    println!("The largest number is {}", largest);
 }
